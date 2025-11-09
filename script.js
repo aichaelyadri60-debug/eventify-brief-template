16941b2f353c9ad2f2c38e3456c7965691b42382
aicha_elya <aichaelyadri60@gmail.com>
2025-11-09 17:39:15 +0100

    Correction des fonctions supprimer et afficherarchive

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-
-
 function switchScreen(screenId) {
     // TODO:
     const Bouttons =document.querySelectorAll(".sidebar__btn");
@@ -229,3 +227,22 @@ function supprimerevent(index){
 
 
 }
+
+function afficherarchive(){
+  const archives =JSON.parse(localStorage.getItem("archives"))||[];
+  const body =document.querySelector(".table__body__archives");
+   body.innerHTML = "";
+  archives.forEach((a,index) => {
+    body.innerHTML +=`<tr class="table__row" data-event-id="${index}">
+                                    <td>${index+1}</td>
+                                    <td>${a.titre}</td>
+                                    <td>${a.place}</td>
+                                    <td>${a.prix}</td>
+                                    <td>
+                                        <button class="btn btn--small" data-action="restore" data-event-id="${index}" onclick="restaurerArchive(${index})">Restore</button>
+                                    </td>
+                          </tr>`
+  });
+
+}
+
